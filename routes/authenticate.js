ad4d79aa244f0e62d81ea0c4563c16a2fa24c656
Saptarshi Dey <saptarshidey.2009@rediffmail.com>
2017-09-21 23:02:47 +0530

    Passport strategy added for authentication

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -1,2 +1,7 @@
 var express = require('express')
 var app = express()
+
+var passport=require('passport')
+var GoogleAuth = require('passport-google-oauth20').Strategy
+
+passport.use(new GoogleAuth())
