c1d9134a6bce779b1a84edcc6d4b3844a73ff0f5
Saptarshi Dey <saptarshidey.2009@rediffmail.com>
2017-09-21 22:54:36 +0530

    Initial server set-up and repo structure

---
+++ index.js
@@ -0,0 +1,15 @@
+var express = require('express')
+var app = express()
+const path = require('path')
+var bodyParser = require('body-parser')
+var port = process.env.PORT || 3000;
+var authenticate = require('./routes/authenticate')
+
+app.use(bodyParser.urlencoded({ extended:true}))
+app.use(bodyParser.json())
+
+
+
+
+app.listen(port)
+console.log("**listening on port: "+port+"**")
