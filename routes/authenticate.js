477b685c4f044097a8aa845b16241038d4406c0b
Saptarshi Dey <dey7.kol@gmail.com>
2017-09-22 23:10:40 +0530

    google oauth credentials added

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -3,5 +3,11 @@ var app = express()
 
 var passport=require('passport')
 var GoogleAuth = require('passport-google-oauth20').Strategy
+var config = require('../config')
 
-passport.use(new GoogleAuth())
+
+passport.use(new GoogleAuth({
+    clientID:config.clientID,
+    clientSecret:config.clientSecret,
+    callbackURL:'/auth/google/callback'
+}))
