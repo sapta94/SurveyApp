56b5fa3644d90071f19325d3f35642501af16364
Saptarshi Dey <dey7.kol@gmail.com>
2017-09-23 20:22:41 +0530

    Succesfull callback handling after verification from google' ;

--- routes/authenticate.js
+++ routes/authenticate.js
@@ -5,6 +5,7 @@ var passport = require("passport");
 var GoogleAuth = require("passport-google-oauth20").Strategy;
 var config = require("../config");
 
+//define gogole strategy with passport
 passport.use(
   new GoogleAuth(
     {
@@ -12,17 +13,24 @@ passport.use(
       clientSecret: config.clientSecret,
       callbackURL: "/auth/google/callback"
     },
-    function(accessToken) {
+    function(accessToken,refreshToken,profile,done) { //callback after getting authenticated by google
       console.log(accessToken);
+      console.log(profile)
     }
   )
 );
 
+//route handler to take user to google server
 router.get(
   "/auth/google",
   passport.authenticate("google", {
     scope: ["profile", "email"]
   })
 );
+
+//route handler to handle callback after verification
+router.get('/auth/google/callback',passport.authenticate('google'))
+
+
 
 module.exports=router;
