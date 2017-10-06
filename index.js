b07f88a6e6e08357ae3a0484b4ad717f84c01dee
Saptarshi Dey <dey7.kol@gmail.com>
2017-10-06 22:26:49 +0530

    Monggose set up and connection with mlab

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ var app = express()
 const path = require('path')
 const passportConfig = require('./services/passport')
 var bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+var config = require("./config");
 var port = process.env.PORT || 5000;
 var authenticate = require('./routes/authenticate')
+
+mongoose.connect(config.mongoUri)
 
 require('./routes/authenticate')(app)
 
