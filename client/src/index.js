af4d2f7f5d35a18b7ee4301e06217b97c2a59406
Saptarshi Dey <dey7.kol@gmail.com>
2017-10-18 22:21:45 +0530

    store and provider added from redux

--- client/src/index.js
+++ client/src/index.js
@@ -1,5 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './components/App'
+import {Provider} from 'react-redux'
+import {createStore,applyMiddleware} from 'react'
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const store = createStore(()=>[],{},applyMiddleware())
+
+ReactDOM.render(
+<Provider store={store}><App /></Provider>, 
+document.getElementById("root"));
