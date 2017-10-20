e930c9768987314360890b36e3d03df7cef60af3
Saptarshi Dey <dey7.kol@gmail.com>
2017-10-20 19:54:32 +0530

    installing materialize css and styling the header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import {BrowserRouter,Route} from 'react-router-dom'
 
+import Header from './Header'
 const Landing = ()=> <h2>Landing</h2>
+const Dashboard = () => <h2>Dashboard</h2>
+const SurveyNew = () => <h2>Survey New</h2>
 
 const App= function(){
     return(
         <div>
            <BrowserRouter>
                 <div>
-                    <Route path="/" component={Landing} />
+                    <Header />
+                    <Route exact path="/" component={Landing} />
+                    <Route exact path="/survey" component={Dashboard} />
+                    <Route exact path="/survey/new" component={SurveyNew} />
                 </div>
            </BrowserRouter>
         </div>
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,9 @@
+import 'materialize-css/dist/css/materialize.min.css'
 import React from "react";
 import ReactDOM from "react-dom";
 import App from './components/App'
 import {Provider} from 'react-redux'
-import {createStore,applyMiddleware} from 'react'
+import {createStore,applyMiddleware} from 'redux'
 import reducers from './reducers'
 
 const store = createStore(()=>reducers,{},applyMiddleware())
