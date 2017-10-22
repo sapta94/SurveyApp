569d0ce4d9dc75df05dd95ef1c6c4fae7c2a446f
Saptarshi Dey <dey7.kol@gmail.com>
2017-10-22 19:15:07 +0530

    configuring auth reducer to handle different case of user login

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 import {FETCH_USER} from './types'
 
 export const fetchUser = () => {
-    
+
     return async function(dispatch){
         const res = await axios.get('/api/currentUser')
-        dispatch({type: FETCH_USER,payload:res})
+        dispatch({type: FETCH_USER,payload:res.data})
     }
 }
