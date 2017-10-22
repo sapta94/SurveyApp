ae9d2ef3e18b632a3c9ca9460a9d9cd408c537a6
Saptarshi Dey <dey7.kol@gmail.com>
2017-10-22 19:11:47 +0530

    code refactoring and use of async-await

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -2,9 +2,9 @@ import axios from 'axios'
 import {FETCH_USER} from './types'
 
 export const fetchUser = () => {
-    return function(dispatch){
-        axios.get('/api/currentUser').then(
-            res => dispatch({type: FETCH_USER,payload:res})
-        )
+    
+    return async function(dispatch){
+        const res = await axios.get('/api/currentUser')
+        dispatch({type: FETCH_USER,payload:res})
     }
 }
