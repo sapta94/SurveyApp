402bbf16a1526e9585906fe6f1622a43594fe9cc
Saptarshi Dey <dey7.kol@gmail.com>
2017-10-27 23:07:26 +0530

    PAyment button added with stripe ui

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,6 +1,7 @@
 import React,{Component} from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
+import Payments from './Payments'
 
 class Header extends Component{
     renderContent(){
@@ -22,7 +23,10 @@ class Header extends Component{
             case false:
                 return <li><a href="/auth/google">Click to Login with Google</a></li>
             default:
-                return <li><a href="/auth/logout">Logout</a></li>
+                return [
+                <li><Payments /></li>,
+                <li><a href="/auth/logout">Logout</a></li>
+            ]
         }
 
     }
