6e556c46b390be139a208c6fe239ae8de08c8558
Saptarshi Dey <dey7.kol@gmail.com>
2017-10-28 22:06:32 +0530

    api route handler and creating a stripe charge object

--- routes/billing.js
+++ routes/billing.js
@@ -1,7 +1,13 @@
 var config=require('../config')
 var stripe=require('stripe')(config.stripeSecretKey)
 module.exports=function(app){
-    app.post('/api/stripe',function(req,res){
-
+    app.post('/api/stripe',async function(req,res){
+        const charge = await stripe.charges.create({
+            amount:500,
+            currency:'usd',
+            description:'$5 for 5 credits',
+            source:req.body.id
+        })
+        console.log(charge)
     })
 }
