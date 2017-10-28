e1760d8e8fec23d2b24733bcb1ed5bfc2310f02c
Saptarshi Dey <dey7.kol@gmail.com>
2017-10-28 20:06:08 +0530

    action creator added to handle token received token back from stripe

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -8,3 +8,10 @@ export const fetchUser = () => {
         dispatch({type: FETCH_USER,payload:res.data})
     }
 }
+
+export const handleToken=(token)=>{
+    return async function(dispatch){
+        const res = await axios.post('/api/stripe',token)
+        dispatch({type: FETCH_USER,payload:res.data})
+    }
+}
