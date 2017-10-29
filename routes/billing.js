c46da99588ebb8e3ea790271d9ee1a3496b8330f
Saptarshi Dey <dey7.kol@gmail.com>
2017-10-29 22:32:12 +0530

    check added if an user is logged in or not

--- routes/billing.js
+++ routes/billing.js
@@ -1,13 +1,18 @@
 var config=require('../config')
 var stripe=require('stripe')(config.stripeSecretKey)
+var requireUser = require('./requireUser')
 module.exports=function(app){
-    app.post('/api/stripe',async function(req,res){
+    app.post('/api/stripe',requireUser,async function(req,res){
         const charge = await stripe.charges.create({
             amount:500,
             currency:'usd',
             description:'$5 for 5 credits',
             source:req.body.id
         })
-        console.log(charge)
+
+        req.user.credits +=5;
+        const user = await(req.user.save())
+        res.send(user)
     })
+
 }
