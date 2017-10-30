ef7bd33c4186c39559a78d5abac8a0c836ccf5aa
Saptarshi Dey <dey7.kol@gmail.com>
2017-10-30 22:47:43 +0530

    logic added for handling routes in production

--- index.js
+++ index.js
@@ -30,6 +30,12 @@ app.use(passport.session());
 require('./routes/authenticate')(app)
 require('./routes/billing')(app)
 
+if(process.env.NODE_ENV==='production'){
+    app.use(express.static('client/build'))
+    const path = require('path')
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+}
+
 
 //app.use('/',authenticate);
 
