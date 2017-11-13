a9e990b3ab75f1d2e3b4e39c68eaf967569895c1
Saptarshi Dey <dey7.kol@gmail.com>
2017-11-13 23:15:34 +0530

    added floating button to create surveys on click

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -2,11 +2,14 @@ import React,{Component} from 'react'
 
 class Dashboard extends Component{
     render(){
-        return(
-            <div>
-                <h3>Dashboard</h3>
+        return <div>
+            <h3>Dashboard</h3>
+            <div className="fixed-action-btn">
+              <a className="btn-floating btn-large red">
+                <i className="large material-icons">add</i>
+              </a>
             </div>
-        )
+          </div>;
     }
 }
 
