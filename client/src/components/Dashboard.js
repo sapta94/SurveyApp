7cf6e65914534c40939a485c85a710777b5b2e40
Saptarshi Dey <dey7.kol@gmail.com>
2017-11-14 23:10:00 +0530

    routing changed

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,13 +1,14 @@
 import React,{Component} from 'react'
+import {Link} from 'react-router-dom'
 
 class Dashboard extends Component{
     render(){
         return <div>
             <h3>Dashboard</h3>
             <div className="fixed-action-btn">
-              <a className="btn-floating btn-large red">
+              <Link to='/survey/new' className="btn-floating btn-large red">
                 <i className="large material-icons">add</i>
-              </a>
+              </Link>
             </div>
           </div>;
     }
