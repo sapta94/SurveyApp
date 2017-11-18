3f836f8a1dec349ce3308711c38e372fe1732522
Saptarshi Dey <dey7.kol@gmail.com>
2017-11-18 21:50:54 +0530

    survey form modified

--- client/src/components/Surveys/SurveyForm.js
+++ client/src/components/Surveys/SurveyForm.js
@@ -1,13 +1,23 @@
 import React,{Component} from 'react';
 import {reduxForm,Field} from 'redux-form'
+import SurveyField from './SurveyField'
 
 class SurveyForm extends Component{
+    renderFileds(){
+        return(
+            <div>
+                <Field label="Survey Tilte" type="text" name="title" component={SurveyField} />
+                <Field label="Subject Line" type="text" name="subject" component={SurveyField} />
+                <Field label="Email Body" type="text" name="body" component={SurveyField} />
+                <Field label="Recipients List" type="text" name="emails" component={SurveyField} />
+            </div>
+        )
+    }
     render(){
         return(
             <div>
                 <form onSubmit={this.props.handleSubmit((values)=>console.log(values))}>
-                    <Field type="text" name="surveyTitle" component="input"/>
-                    <Field type="password" name="surveyTitle" component="input"/>
+                    {this.renderFileds()}
                 <button type="submit">Submit</button>
                 </form>
             </div>
